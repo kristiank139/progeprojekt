089bfdec85c8048aa9f66b2fbfa3657041ef97c1
Arto Reinik <artoreinik@gmail.com>
2024-09-10 12:51:58 +0300

    Rakendus töötab

--- src/mol/app/models.py
+++ src/mol/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Küsimus(models.Model):
+    küsimus_tekst = models.Charfield(max_length=200)
+
+class Valikvastus(models.Model):
+    küsimus = models.ForeignKey(Küsimus, on_delete=models.CASCADE)
