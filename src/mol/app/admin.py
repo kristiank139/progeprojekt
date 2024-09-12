11bc0f7ad4ac49a40490bd6780189a00f7fc276f
kristiank139 <105385905+kristiank139@users.noreply.github.com>
2024-09-12 21:07:34 +0300

    Modelid on olemas

--- src/mol/app/admin.py
+++ src/mol/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Küsimus, Valikvastus
+
+admin.site.register(Küsimus)
+admin.site.register(Valikvastus)
--- src/mol/app/models.py
+++ src/mol/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Küsimus(models.Model):
-    küsimus_tekst = models.Charfield(max_length=200)
+    küsimus_tekst = models.CharField(max_length=200)
 
 class Valikvastus(models.Model):
     küsimus = models.ForeignKey(Küsimus, on_delete=models.CASCADE)
