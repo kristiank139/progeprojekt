91eafaf9fcde6be889ae72c8ac1287f8f484c03d
MikkRK <114816831+MikkRK@users.noreply.github.com>
2024-09-14 19:44:00 +0300

    Lisasin django-latexify

--- src/mol/app/views.py
+++ src/mol/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from . import urls
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('test.html')
+    return HttpResponse(template.render())
