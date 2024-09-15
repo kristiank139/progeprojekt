8631a360195984e8d442011c1de728187082f45f
kristiank139 <105385905+kristiank139@users.noreply.github.com>
2024-09-15 15:33:08 +0300

    Staatilised failid eraldi folderis

--- src/mol/app/views.py
+++ src/mol/app/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('test.html')
+    template = loader.get_template('app/base.html')
     return HttpResponse(template.render())
