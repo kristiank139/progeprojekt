0e27a7ddd45146e406c367ec5ded06c81dd5ee20
Arto Reinik <artoreinik@gmail.com>
2024-09-24 12:51:18 +0300

    Funktsioon csv failist lugemiseks

--- src/mol/app/models.py
+++ src/mol/app/models.py
@@ -6,4 +6,5 @@ class Küsimus(models.Model):
     küsimus_tekst = models.CharField(max_length=200)
 
 class Valikvastus(models.Model):
+    vastus_tekst = models.CharField(max_length=200)
     küsimus = models.ForeignKey(Küsimus, on_delete=models.CASCADE)
--- src/mol/app/views.py
+++ src/mol/app/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from . import urls
 from django.http import HttpResponse
 from django.template import loader
+import csv
+
+def get_questions(id): # Hakkab lugema csv failist teemale vastavad küsimused
+    with open('test.csv', newline="") as küsimused:
+        lugeja = csv.reader(küsimused, delimiter=" ", quotechar="|")
+        for rida in lugeja:
+            if " ".join(rida)[0].isnumeric() and int(" ".join(rida)[0]) == id: # Kontrollib et ei ole esimene rida ja kas on tahetud id
+                print((" ".join(rida)).split(";")[1])
 
 def index(request):
+    get_questions(2)
     template = loader.get_template('app/base.html')
     return HttpResponse(template.render())
