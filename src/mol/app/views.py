15cdcf42197cc8b446e7dc6afea98608ecbbff1b
Arto Reinik <artoreinik@gmail.com>
2024-10-01 12:19:40 +0300

    Funktsionaalsuse arendamine
    Pooleli

--- src/mol/app/views.py
+++ src/mol/app/views.py
@@ -7,11 +7,13 @@ import csv
 def get_questions(id): # Hakkab lugema csv failist teemale vastavad küsimused
     with open('test.csv', newline="") as küsimused:
         lugeja = csv.reader(küsimused, delimiter=" ", quotechar="|")
+        küsimused = []
         for rida in lugeja:
             if " ".join(rida)[0].isnumeric() and int(" ".join(rida)[0]) == id: # Kontrollib et ei ole esimene rida ja kas on tahetud id
-                print((" ".join(rida)).split(";")[1])
+                küsimused.append((" ".join(rida)).split(";")[1])
 
-def index(request):
-    get_questions(2)
-    template = loader.get_template('app/base.html')
-    return HttpResponse(template.render())
+        return küsimused
+
+def index(request, id):
+    küsimused = get_questions(id)
+    return render(request, 'app/base.html',{"küsimused": küsimused})
