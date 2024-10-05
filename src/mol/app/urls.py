a15b46c8ee055ae49a3cc02c80d7f972bd54c5db
kristiank139 <105385905+kristiank139@users.noreply.github.com>
2024-10-05 20:51:28 +0300

    Arendasin flashcardide süsteemi
    Nüüd tulevad exceli failidest valemid erinevate teemade kohta igal lehel eraldi - leht on nimetatud exceli faili järgi nt. calculus.csv jaoks on leht /app/Calculus. Kaartide vahel saab liikuda edasi ja tagasi

--- src/mol/app/urls.py
+++ src/mol/app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:id>/", views.index, name="index"),
+    path("<str:aine>", views.index, name="index"),
 ]
--- src/mol/app/views.py
+++ src/mol/app/views.py
@@ -4,16 +4,23 @@ from django.http import HttpResponse
 from django.template import loader
 import csv
 
-def get_questions(id): # Hakkab lugema csv failist teemale vastavad küsimused
-    with open('test.csv', newline="") as küsimused:
-        lugeja = csv.reader(küsimused, delimiter=" ", quotechar="|")
-        küsimused = []
+def get_questions(aine): # Hakkab lugema csv failist teemale vastavad küsimused
+    with open(f'data/{aine}.csv', newline="") as küsimused:
+        lugeja = csv.reader(küsimused, delimiter=";", quotechar="|")
+        küsimused = {}
+        i = 0
         for rida in lugeja:
-            if " ".join(rida)[0].isnumeric() and int(" ".join(rida)[0]) == id: # Kontrollib et ei ole esimene rida ja kas on tahetud id
-                küsimused.append((" ".join(rida)).split(";")[1])
+            #if " ".join(rida)[0].isnumeric(): # Kontrollib et ei ole esimene rida
+            #    küsimused[i] = (" ".join(rida)).split(";")[1]
+            #    i += 1
+            küsimused[i] = rida[2]
+            i += 1
+            print(rida[2])
+
+        print(küsimused)
 
         return küsimused
 
-def index(request, id):
-    küsimused = get_questions(id)
-    return render(request, 'app/base.html',{"küsimused": küsimused})
+def index(request, aine):
+    küsimused = get_questions(aine)
+    return render(request, 'app/base.html', {"küsimused": küsimused})
