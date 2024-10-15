f8200ad9e376e96f20efb367a294eccbef35aa68
MikkRK <114816831+MikkRK@users.noreply.github.com>
2024-10-15 21:50:07 +0300

    added updated pythonanywhere, and fixed bugs

--- src/mol/app/context_processors.py
+++ src/mol/app/context_processors.py
@@ -1,5 +1,8 @@
 import os
-categories = [x.split('.')[0] for x in os.listdir("data/flashcards")]
+from django.conf import settings  # Import settings
 
-def categories_processor(request):            
+flashcards_dir = os.path.join(settings.BASE_DIR, 'data', 'flashcards')
+categories = [x.split('.')[0] for x in os.listdir(flashcards_dir)]
+
+def categories_processor(request):
     return {'categories': categories}
